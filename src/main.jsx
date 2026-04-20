685fb995d19cb51c9284d9af9cf5f2318da13861
Eungsoo <rladmdtn01010@gmail.com>
2026-04-21 02:22:13 +0900

    오류 수정

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(console.error);
+  });
+}
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
